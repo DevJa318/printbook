dfb948ce5bfcfc39d8ed92855ab2da971ef21ea1
DevJa318 <rkipek@gmail.com>
2023-12-29 10:30:06 +0100

    first version - make pdf to print book

--- print_books.py
+++ print_books.py
@@ -1,33 +1,53 @@
-import sys
+#!/bin/env python
 
+# import sys
+from pypdf import PdfWriter, PdfReader
 
-ilosc_stron = int(sys.argv[1])
-if ilosc_stron%4 == 0:
-	pod4 = ilosc_stron
+dokument = "/home/devja318/Pulpit/styleguidebash.pdf"
+reader = PdfReader(dokument)
+
+number_of_pages  = len(reader.pages)
+
+if number_of_pages%4 == 0:
+	pod4 = number_of_pages
 else:
-	mod4 = 4-ilosc_stron%4
-	pod4 = ilosc_stron+mod4
+	mod4 = 4-number_of_pages%4
+	pod4 = number_of_pages+mod4
 
 x = [i+1 for i in range(pod4)]
 
-stronytyl = len(x)-1
-stronyprzod = 0
+do_until = int(len(x)/4)
+book_pages = []
 
-dountil = int(len(x)/4)
+pages_back = len(x)-1
+pages_front = 0
+for i in range(do_until):
+	book_pages.append(x[pages_back])
+	book_pages.append(x[pages_front])
+	pages_back = pages_back - 2
+	pages_front = pages_front + 2
 
-print("Część pierwsza: ", end='')
-for i in range(dountil):
-	print(x[stronytyl], end=', ')
-	print(x[stronyprzod], end=', ')
-	stronytyl = stronytyl - 2
-	stronyprzod = stronyprzod + 2
+pages_front = int(len(x)/2-1)
+pages_back = int(len(x)/2)
+for i in range(do_until):
+	book_pages.append(x[pages_front])
+	book_pages.append(x[pages_back])
+	pages_front = pages_front - 2
+	pages_back = pages_back + 2 
 
-stronyprzod = int(len(x)/2-1)
-stronytyl = int(len(x)/2)
-print("\nCzęść druga: ", end='')
-for i in range(dountil):
-	print(x[stronyprzod], end=', ')
-	print(x[stronytyl], end=', ')
-	stronyprzod = stronyprzod - 2
-	stronytyl = stronytyl + 2 
-print()
+merger = PdfWriter()
+input = open(dokument, "rb")
+
+print(book_pages)
+for i in book_pages:
+	try:
+		merger.append(input, [i-1])
+	except:
+		merger.append(input, [0])
+
+output = open("document-output.pdf", "wb")
+merger.write(output)
+
+merger.close()
+output.close()
+
